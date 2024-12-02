e43ed44997bd5d782142f59bb0f880e0674b2808
John Dojjy Yeung <p80246@gmail.com>
2024-12-02 15:45:37 +0800

    Finished voice changing function, discard callback approach to make the pipeline more flexible. Voice change function will be called every second triggered by a timer

--- app_ui.py
+++ app_ui.py
@@ -6,6 +6,8 @@ from audio_processor import AudioProcessor
 from media_manager import MediaManager
 from ui_status import VoiceChangerStatus
 
+import numpy as np
+
 class AppMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -14,6 +16,10 @@ class AppMainWindow(QMainWindow):
         self.audio_processor = AudioProcessor()
         self.audio_processor.start_stream()
         uic.loadUi(Path("ui", "control.ui"), self)
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.change_voice)
+        self.timer.start(1000)
 
     @QtCore.pyqtSlot()
     def on_mic_btn_clicked(self):
@@ -43,3 +49,16 @@ class AppMainWindow(QMainWindow):
     def on_octave_slider_valueChanged(self, value):
         """Value should be 12-24 with a step of 2"""
         self.octave_value.setText(f"{value}")
+
+    def change_voice(self):
+        """Supplied to Audio in/out stream as callback"""
+        input_audio = self.audio_processor.read_from_input_stream().T.squeeze(0)
+        input_audio *= 5 # make the input louder
+        if not self.vc_status.mic_on:
+            return
+        if self.vc_status.denoising:
+            input_audio = self.audio_processor.extract_freq(input_audio, self.n_steps_slider.value() / 10)
+        if self.vc_status.voice_changing:
+            input_audio = self.audio_processor.change_voice(input_audio, self.n_steps_slider.value() / 10, self.octave_slider.value())
+        self.audio_processor.write_to_output_stream(np.expand_dims(input_audio, 0).T)
+        self.audio_intensity_bar.setValue(int(input_audio.max() * 100))
--- audio_processor.py
+++ audio_processor.py
@@ -1,6 +1,7 @@
 from typing import Any, Callable
 import numpy as np
 import librosa
+import resampy # This was lazily loaded in librosa, load it first before overhead when calling librosa function
 import matplotlib.pyplot as plt
 from PyQt5 import QtCore
 import sounddevice as sd
@@ -13,13 +14,22 @@ class AudioProcessor:
             samplerate=sr,
             blocksize=sr,
             channels=1, # single channel only
-            callback=self.change_voice,
         )
+        self.sr = sr
         dt = 1/sr
         t = np.arange(0, 1, dt)
         self.window_length = len(t)
         self.frequency = (1 / (dt * self.window_length)) * np.arange(self.window_length)
         self.L = np.arange(1, np.floor(self.window_length / 2), dtype=np.int64)
+
+    def read_from_input_stream(self):
+        """Read frames from the stream and return them, output shape is (frames, channels), in our case channel is 1"""
+        frames, status = self.stream.read(self.sr)
+        return frames
+
+    def write_to_output_stream(self, data: np.ndarray):
+        """Write data to the output stream, shape should be (frames, channels), in our case channel is 1"""
+        underflowed = self.stream.write(data)
 
     def start_stream(self):
         """This method starts both input and output audio stream"""
@@ -46,9 +56,11 @@ class AudioProcessor:
         return np.fft.ifft(fhat).real
 
     def smooth(self, data: np.ndarray):
+        """Smooth the audio data by applying a convolve filter"""
         # [0.25, 0.5, 0.25], [0.05, 0.2, 0.5, 0.2, 0.05]
-        gaussian = np.array([0.05, 0.2, 0.5, 0.2, 0.05])
-        average = np.array([1, 1, 1, 1, 1, 1, 1])/7
+        # need to be float32 to match sounddevice stream
+        gaussian = np.array([0.05, 0.2, 0.5, 0.2, 0.05], dtype=np.float32)
+        average = np.array([1, 1, 1, 1, 1, 1, 1], dtype=np.float32) / 7
         return np.convolve(data, average, "same")
 
     #Extract Freq in similar to remove noise
@@ -82,5 +94,23 @@ class AudioProcessor:
 
         return np.fft.ifft(fhat2).real
 
-    def change_voice(self, indata, outdata, frames, time, status):
-        """Supplied to sounddevice stream as callback"""
+    def change_voice(self, data: np.ndarray, n_steps: float, bins_per_octave: int):
+        """apply voice change algorithm
+
+        Args:
+            data (np.ndarray): transformed audio stream data of shape (frames,)
+            n_steps (float): multiplier to the pitch level
+            bins_per_octave (int): adder to the pitch level
+
+        Returns:
+            frequency changed audio stream data of shape (frames,): _description_
+        """
+        base_freq_shift = librosa.effects.pitch_shift(
+            data, sr=self.sr, n_steps=n_steps, bins_per_octave=bins_per_octave, res_type="kaiser_fast"
+        )
+        harmonic_freq_shift = librosa.effects.pitch_shift(
+            data, sr=self.sr, n_steps=n_steps*1.5, bins_per_octave=bins_per_octave, res_type="kaiser_fast"
+        )
+        data = 0.25 * base_freq_shift + 0.75 * harmonic_freq_shift
+        data = self.smooth(data)
+        return data
